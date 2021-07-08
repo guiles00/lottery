35b64fc7a7b12d0cecdb31c31e2f4a01f1af92c8
Guillermo Caserotto <gcaserotto@tsjbaires.gov.ar>
2021-07-08 13:37:36 -0300

    final

---
+++ lottery/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "size mistake word angry choose twice story someone divorce quick weather depth",
+  "https://rinkeby.infura.io/v3/f68b569cbf5f4e989d5a9e926e1e20d0"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log(interface);
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
